9f971217fc26876122646c01af980dcad82bc0ad
Shashank Hiremath <shashankhiremath73@gmail.com>
2023-01-18 11:33:40 +0530

    set language_level = 2

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     url='github.com/EvgeniDubov/hellinger-distance-criterion',
     author='Evgeni Dubov',
     author_email='evgeni.dubov@gmail.com',
-    ext_modules=cythonize('hellinger_distance_criterion.pyx', 
-                         compiler_directives={'language_level' : "2"}),
+    ext_modules=cythonize('hellinger_distance_criterion.pyx', language_level = "2"),
     include_dirs=[numpy.get_include()]
 )
